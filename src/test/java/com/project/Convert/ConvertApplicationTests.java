892573b57df69e140ab7fd847105879440e588a1
akshayakaushik <kaushikakshaya03@gmail.com>
2020-11-24 09:55:12 +0530

    New File Coverter Project

---
+++ src/test/java/com/project/Convert/ConvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Convert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
